c72d3e528f67e1534d5330bcef577d44fabd38d8
marhel28 <ammarmaulahela20@gmail.com>
2026-01-12 19:41:24 +0700

    push data

--- config.py
+++ config.py
@@ -1,17 +1,20 @@
 from functools import lru_cache
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import BaseSettings,SettingsConfigDict
+from dotenv import load_dotenv
+load_dotenv()
 
 class Settings(BaseSettings):
     DATABASE_URL: str | None = None
     JWT_SECRET_KEY: str | None = None
     JWT_ALGORITHM: str = "HS256"
 
+
     model_config = SettingsConfigDict(
-        env_file=".env",          # untuk lokal saja
+        env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore"
-    )
+    )    
+    
 
-@lru_cache
-def Config() -> Settings:
-    return Settings()
+
+Config = Settings()
